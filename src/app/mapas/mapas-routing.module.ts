aea7044d4818c64c1b2bd53629a110aa7469a771
AlejandroRaya09 <alejandroraya09@hotmail.com>
2023-01-30 19:28:27 -0600

    LazyLoad y Menu Simple

--- src/app/mapas/mapas-routing.module.ts
+++ src/app/mapas/mapas-routing.module.ts
@@ -1,7 +1,27 @@
-import { NgModule } from '@angular/core';
+import { NgModule,  } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FullScreenComponent } from './pages/full-screen/full-screen.component';
+import { ZommRangeComponent } from './pages/zomm-range/zomm-range.component';
+import { MarcadoresComponent } from './pages/marcadores/marcadores.component';
+import { PropiedadesComponent } from './pages/propiedades/propiedades.component';
 
-const routes: Routes = [];
+
+//lazyload
+const routes: Routes = [
+{
+  path: '',
+  children:[
+    {path:'fullscreen', component: FullScreenComponent},
+    {path:'zoom-range', component: ZommRangeComponent},
+    {path:'marcadores', component: MarcadoresComponent},
+    {path:'propiedades', component: PropiedadesComponent},
+    {path:'**', redirectTo: 'fullscreen'}
+  ]
+}
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
